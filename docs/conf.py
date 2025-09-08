9c19809bc99dcdc52fd7c5845533a90b8d16b833
Lachlan Deakin <ljdgit@gmail.com>
2025-09-08 11:27:24 +1000

    pre-commit

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,24 @@
 # Sphinx configuration for anu-ctlab-io docs
-project = 'anu-ctlab-io'
-author = 'MaterialsPhysicsANU' # FIXME
-release = '0.2.0' # FIXME: Auto?
+project = "anu-ctlab-io"
+author = "MaterialsPhysicsANU"  # FIXME
+release = "0.2.0"  # FIXME: Auto?
 extensions = [
     # 'myst_parser',
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
 ]
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 autosummary_generate = True
 source_suffix = {
     # '.md': 'markdown',
-    '.rst': 'restructuredtext',
+    ".rst": "restructuredtext",
 }
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-    'inherited-members': True,
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "inherited-members": True,
 }
