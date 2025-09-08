be9c460fafdc1e260880792cb2c33c524aadb21d
Lachlan Deakin <ljdgit@gmail.com>
2025-09-08 15:29:20 +1000

    feat: split zarr/netcdf as optional dependency groups
    - remove unused `hidefix` dependency
      - Consumers can still use this if they want via kwargs passed to `open_mfdataset`

--- src/anu_ctlab_io/__init__.py
+++ src/anu_ctlab_io/__init__.py
@@ -4,6 +4,13 @@ from anu_ctlab_io._dataset import Dataset
 from anu_ctlab_io._datatype import DataType, StorageDType
 from anu_ctlab_io._version import version as __version__
 from anu_ctlab_io._voxel_properties import VoxelSize, VoxelUnit
+from contextlib import suppress
+
+with suppress(ImportError):
+    import anu_ctlab_io.netcdf as netcdf
+
+with suppress(ImportError):
+    import anu_ctlab_io.zarr as zarr
 
 __all__ = [
     "VoxelSize",
