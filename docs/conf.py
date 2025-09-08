026eb155672f7358d7e433f67c70253d476d5a73
Lachlan Deakin <ljdgit@gmail.com>
2025-09-08 11:24:24 +1000

    chore: add minimal docs

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# Sphinx configuration for anu-ctlab-io docs
+project = 'anu-ctlab-io'
+author = 'MaterialsPhysicsANU' # FIXME
+release = '0.2.0' # FIXME: Auto?
+extensions = [
+    # 'myst_parser',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+]
+html_theme = 'sphinx_rtd_theme'
+autosummary_generate = True
+source_suffix = {
+    # '.md': 'markdown',
+    '.rst': 'restructuredtext',
+}
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'inherited-members': True,
+}
