564d97946236bc1bc85ba7baba35fb27b7941ee6
Lachlan Deakin <ljdgit@gmail.com>
2025-09-11 12:11:33 +1000

    fix: actually run the datatype tests

--- tests/test_datatype.py
+++ tests/test_datatype.py
@@ -1,13 +1,19 @@
+import pytest
+
 from anu_ctlab_io._datatype import DataType
 
 
-def from_basename():
+def test_from_basename():
     assert DataType.from_basename("tomo_float") == DataType.TOMO_FLOAT
     assert DataType.from_basename("tomo") == DataType.TOMO
+    with pytest.raises(ValueError):
+        DataType.from_basename("nonsense")
 
 
-def infer_from_path():
+def test_infer_from_path():
     assert (
         DataType.infer_from_path("/path/to/tomo_floatData.zarr") == DataType.TOMO_FLOAT
     )
     assert DataType.infer_from_path("/path/to/tomoData.zarr") == DataType.TOMO
+    with pytest.raises(ValueError):
+        DataType.from_basename("/path/to/nonsense")
