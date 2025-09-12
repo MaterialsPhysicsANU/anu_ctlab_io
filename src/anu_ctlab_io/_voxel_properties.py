6c6be0e483c42fe49ce597f819016ac4103dac0f
omdowley <owen@dowleys.com>
2025-09-12 12:49:33 +1000

    fix: update VoxelUnit to fully match mango types

--- src/anu_ctlab_io/_voxel_properties.py
+++ src/anu_ctlab_io/_voxel_properties.py
@@ -7,34 +7,45 @@ class UnknownVoxelUnitException(Exception):
 
 
 class VoxelUnit(Enum):
-    """
-    The unit of size of a voxel.
-    TODO: double check mango supported units
-    """
+    """The unit of size of a voxel."""
 
     m = auto()
+    cm = auto()
     mm = auto()
     um = auto()
     nm = auto()
+    angstrom = auto()
+    voxel = auto()
 
     @classmethod
     def from_str(cls, string: str) -> "VoxelUnit":
+        """Create a VoxelUnit from the string name of the unit.
+
+        Accepts a wide range of standard representations of each unit, and is case insensitive."""
         units_lut: dict[str, VoxelUnit] = {
             # short names
             "m": cls.m,
+            "cm": cls.cm,
             "mm": cls.mm,
             "um": cls.um,
             "nm": cls.nm,
+            "a": cls.angstrom,
             # long names
             "meter": cls.m,
+            "centimeter": cls.cm,
             "millimeter": cls.mm,
             "micrometer": cls.um,
             "nanometer": cls.nm,
+            "angstrom": cls.angstrom,
+            "voxel": cls.voxel,
             # alternative symbols
             "µm": cls.um,
+            "å": cls.angstrom,
+            "au": cls.angstrom,
+            "a.u.": cls.angstrom,
         }
         try:
-            return units_lut[string]
+            return units_lut[string.lower()]
         except KeyError as e:
             raise UnknownVoxelUnitException(f"Unknown VoxelUnit {string}", e) from e
 
