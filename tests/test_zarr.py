5d9ed651d107fed3358d5bbe6524a5a8c664bf1f
omdowley <owen@dowleys.com>
2025-09-12 11:32:23 +1000

    fix: fix pre-commit spellcheck
    Also resolves some minor lint issues.

--- tests/test_zarr.py
+++ tests/test_zarr.py
@@ -34,7 +34,7 @@ def test_read_zarr():
 
 
 @pytest.mark.skipif(not _HAS_ZARR, reason="Requires 'zarr' extra")
-def test_read_some_zarr():
+def test_read_ome_zarr():
     dataset = anu_ctlab_io.Dataset.from_path("tests/data/tomoLoRes_SS_AM.zarr")
     assert dataset.dimension_names == ("z", "y", "x")
     assert dataset.voxel_unit == "mm"
