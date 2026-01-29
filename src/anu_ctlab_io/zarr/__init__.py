5fa170dd46656dac542dec82179d95c1544a600e
omdowley <owen@dowleys.com>
2026-01-29 17:18:15 +1100

    fixup! fix: zarr imports causing errors without extra

--- src/anu_ctlab_io/zarr/__init__.py
+++ src/anu_ctlab_io/zarr/__init__.py
@@ -2,6 +2,7 @@
 
 This is an optional extra module, and must be explicitly installed to be used (e.g., ``pip install anu_ctlab_io[zarr]``)."""
 
+import importlib.util
 from collections.abc import Mapping
 from pathlib import Path
 from typing import Any
@@ -13,6 +14,10 @@ import zarr
 from anu_ctlab_io._dataset import Dataset
 from anu_ctlab_io._datatype import DataType
 from anu_ctlab_io._voxel_properties import VoxelUnit
+
+if importlib.util.find_spec("ome_zarr_models") is None:
+    raise ImportError("ome-zarr-models could not be imported.")
+
 from anu_ctlab_io.zarr._writer import dataset_to_zarr
 
 __all__ = ["dataset_from_zarr", "dataset_to_zarr"]
